dc259141ba0955c5f2f68d88f69d9cd05a8e2237
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-02-28 08:43:34 +0100

    Effect review remarks

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-import Book from './Book.js';
-import BookCollection from './BookCollection.js';
+import Book from "./Book.js";
+import BookCollection from "./BookCollection.js";
 
 const bookCollection = new BookCollection();
 
-const container = document.getElementById('contents');
+const container = document.getElementById("contents");
 
 // Add book to collection
 const addBook = () => {
-  const title = document.getElementById('title-input').value;
-  const author = document.getElementById('author-input').value;
+  const title = document.getElementById("title-input").value;
+  const author = document.getElementById("author-input").value;
 
   bookCollection.addBook(new Book(title, author));
 
@@ -24,40 +24,38 @@ const removeBook = (title) => {
 
 // Display all books in the collection
 function displayBooks() {
-  const div = document.createElement('div');
-  div.id = 'book-list';
+  const div = document.createElement("div");
+  div.id = "book-list";
   container.innerHTML = div.outerHTML;
-  const bookList = document.getElementById('book-list');
-  bookList.innerHTML = '';
+  const bookList = document.getElementById("book-list");
+  bookList.innerHTML = "";
 
   // Add the h2 element before the book list
-  const h2 = document.createElement('h2');
-  h2.textContent = 'All Awesome Books';
+  const h2 = document.createElement("h2");
+  h2.textContent = "All Awesome Books";
   container.insertBefore(h2, bookList);
 
   bookCollection.books.forEach((book) => {
-    const bookElement = document.createElement('div');
-    bookElement.classList.add('book-item');
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book-item");
     bookElement.innerHTML = `
     <p><span class="book-title">"${book.title}" </span> <span class="book-author"> 
     by ${book.author} </span><button class="remove-btn" 
     data-ref="${book.title}">Remove</button> </p>
     `;
-    const remBtn = bookElement.querySelector('button');
+    const remBtn = bookElement.querySelector("button");
     remBtn.onclick = () => {
       removeBook(book.title);
     };
     bookList.appendChild(bookElement);
   });
-
-  // container.innerHTML = bookList.outerHTML;
 }
 
 function displayAddNew() {
-  const div = document.createElement('div');
-  div.id = 'add-new-book';
+  const div = document.createElement("div");
+  div.id = "add-new-book";
   container.innerHTML = div.outerHTML;
-  const addNew = document.getElementById('add-new-book');
+  const addNew = document.getElementById("add-new-book");
   addNew.innerHTML = `<span class="divide-line"></span>
       <br><br><br><br><br>
       <h2>Add A New Book</h2>
@@ -68,12 +66,12 @@ function displayAddNew() {
       </form>
       <br><br><br>
       `;
-  addNew.querySelector('button').addEventListener('click', addBook);
+  addNew.querySelector("button").addEventListener("click", addBook);
 }
 
 function displayContact() {
-  const addNew = document.createElement('div');
-  addNew.id = 'add-new-book';
+  const addNew = document.createElement("div");
+  addNew.id = "add-new-book";
   addNew.innerHTML = `<span class="divide-line"></span>
       <h2>Contact Information</h2><br><br>
       <p>Do have any questions or you just want to say "Hello"? <br>
@@ -87,17 +85,14 @@ function displayContact() {
   container.innerHTML = addNew.outerHTML;
 }
 
-// const bookList =
-document.getElementById('books-list').onclick = displayBooks;
-// const addNew =
-document.getElementById('book-add').onclick = displayAddNew;
-// const contact =
-document.getElementById('contact').onclick = displayContact;
+document.getElementById("books-list").onclick = displayBooks;
+document.getElementById("book-add").onclick = displayAddNew;
+document.getElementById("contact").onclick = displayContact;
 
-document.getElementById('today-date').textContent = new Date().toDateString();
+document.getElementById("today-date").textContent = new Date().toDateString();
 
 // select the element where the time will be displayed
-const timeDisplay = document.getElementById('today-time');
+const timeDisplay = document.getElementById("today-time");
 
 // create a function to update the time every second
 function updateTime() {
